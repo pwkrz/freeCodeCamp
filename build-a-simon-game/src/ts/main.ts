88261ef4e600f9250de306515a485988acf99ce1
pwkrz <pg.krzeminski@gmail.com>
2018-04-06 00:28:26 +0200

    implemented basic game logic

--- build-a-simon-game/src/ts/main.ts
+++ build-a-simon-game/src/ts/main.ts
@@ -8,11 +8,17 @@ enum Turn {
     User,
     Comp
 };
+interface UserMove {
+    (e: MouseEvent | TouchEvent, i: number): void;
+};
 class Field {
     private beep: HTMLAudioElement;
+    private timeOut: number;
 
-    constructor(public field: HTMLElement, public index: number){
+    constructor(public field: HTMLElement, index: number, listener: UserMove){
         this.beep = new Audio(BEEPS[index]);
+        
+        this.field.addEventListener("click", (e: MouseEvent | TouchEvent) => listener(e, index), true);
     }
     public on(eventName: string, listener: EventListener, bool: boolean = true) : Field {
         this.field.addEventListener(eventName, listener, bool);
@@ -22,62 +28,115 @@ class Field {
     public toggleClass(className: string, condition: boolean, timeout?: number) : Field {
         if(condition) this.field.classList.add(className);
         if(!condition) this.field.classList.remove(className);
-        if(timeout) setTimeout( () => this.field.classList.toggle(className), 500 );
-
+        if(timeout){
+            setTimeout( () => this.field.classList.toggle(className), timeout );  
+        } 
         return this;
     }
     public blink() : Field {
         this.beep.play()
-        // this.field.classList.add("blink");
-        // setTimeout( () => this.field.classList.remove("blink"), 500 )
-        this.toggleClass("blink", true, 500);
+        this.toggleClass("blink", true, 475);
+        return this;
+    }
+}
+class FieldMap {
+    private fields: Field[];
+
+    constructor(fieldDivs: NodeList, listener: UserMove){
+        this.fields = [];
+
+        Array.prototype.forEach.call( fieldDivs, (div: HTMLElement, i: number) => {
+            let newField: Field = new Field(div, i, listener);
+
+            this.fields.push(newField);
+        })
+    }
+    public getField(index: number): Field {
+        return this.fields[index];
+    }
+    public getLength(): number {
+        return this.fields.length;
+    }
+    public toggleClass(className: string, condition: boolean) : FieldMap {
+
+        this.fields.forEach( (field: Field) => field.toggleClass(className, condition) );
+        
+        return this;
+    }
+    public blink() : FieldMap {
+
+        this.fields.forEach( (field: Field) => field.blink() );
 
         return this;
     }
 }
 class SimonGame {
     private sequence: number[];
-    private fields: Field[];
+    private fields: FieldMap;
+    private clickCounter: number;
 
-    constructor(private fieldDivs: NodeList, private startBtn: HTMLButtonElement, private strictBtn: HTMLButtonElement){
-        this.sequence = [];
-        this.fields = [];
+    constructor(fieldDivs: NodeList, private startBtn: HTMLButtonElement, private strictBtn: HTMLButtonElement){
+        this.fields = new FieldMap(fieldDivs, this.userMove);
         this.init();
     }
-    public userMove(e: MouseEvent | TouchEvent, i: number){
-        this.fields[i].blink();
-
-        
+    public gameEnd(){
+        this.fields.toggleClass("clickable", false);
+        console.log("end")
     }
-    public compMove(){
+    public fail = () => {
+        this.fields.toggleClass("clickable", false).blink();
+
+        setTimeout( () => this.compMove(false), 1000 );
+    }
+    public userMove: UserMove = (e: MouseEvent | TouchEvent, i: number) => {
+
+        this.fields.getField(i).toggleClass("clickable", false);
+
+        let correctClick: boolean = this.sequence[this.clickCounter] === i;
+
+        if (!correctClick) {
+
+            this.fail();
+
+        } else {
+
+            this.fields.getField(i).blink();
+
+            if (this.clickCounter === 5) {
+                this.gameEnd();
+            } else if (this.clickCounter + 1 === this.sequence.length) {
+                this.fields.toggleClass("clickable", false);
+                setTimeout( () => this.compMove(true), 1500 );
+            } else {
+                setTimeout( () => this.fields.getField(i).toggleClass("clickable", true), 500 );
+                this.clickCounter++;
+            }
+        }
+    }
+    public compMove = (nextBeep: boolean) => {
         let len: number = this.sequence.length;
 
         if(len){
             this.sequence.forEach( (fieldIdx: number, i: number) => {
                 setTimeout( () => {
-                    this.fields[fieldIdx].blink()
-                }, i*500 )
+                    this.fields.getField(fieldIdx).blink()
+                }, i*750 )
                 
             });
         }
-        let rand: number = Math.floor( Math.random() * this.fields.length );
-
-        setTimeout( () => this.fields[rand].blink(), (len+1)*500 )
-
-        this.sequence.push(rand);
-
-        this.fields.forEach( (field: Field) => field.toggleClass("clickable", true) )
+        if(nextBeep){
+            let rand: number = Math.floor( Math.random() * this.fields.getLength() );
+    
+            setTimeout( () => this.fields.getField(rand).blink(), (len*750 || 1000) )
+    
+            this.sequence.push(rand);
+        }
+        this.clickCounter = 0;
+        setTimeout( () => this.fields.toggleClass("clickable", true), (len + 1) * 750 );
     }
-    private init(): void {
-        this.fieldDivs.forEach( (div: HTMLElement, i: number) => {
-            let newField: Field = new Field(div, i);
-
-            newField.on("click", (e: MouseEvent | TouchEvent) => this.userMove(e, i) );
-
-            this.fields.push(newField);
-        })
-
-        this.compMove()
+    private init = (): void => {
+        this.sequence = [];
+        this.compMove(true)
     }
 }
 
